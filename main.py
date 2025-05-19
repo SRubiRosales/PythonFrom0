2c768e655412e66d2d27eb0c9ac131529b60aa43
SRubiRosales <sharon_rosales98@hotmail.com>
2025-05-19 11:57:25 -0600

    Data types

--- main.py
+++ main.py
@@ -7,7 +7,27 @@ line breaks
 print("Hello world!, from script in Python")
 
 name = "Código Facilito" # Str
-age = 20 # int
 print(name)
 print(type(name))
+
+# Strings (" - ')
+first_name = "Sharon"
+last_name = 'Rosales'
+print(first_name)
+print(last_name)
+print(type(first_name))
+
+# Integers +-
+age = 20 # int
+number = 100_000_000
 print(type(age))
+print(number)
+
+# Floats +-
+pi = 3.1416
+print(type(pi))
+
+# Booleans (True | False)
+is_active = True
+print(is_active)
+print(type(is_active))
